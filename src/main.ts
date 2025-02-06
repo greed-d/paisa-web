56747221c95a4e9287dd7f1a7cfd2be781bf205b
greed-d <deekshit.bti@gmail.com>
2025-02-06 12:25:44 +0545

    `feat(global type)` : Add global types

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { loginState } from "./pages/login-state";
 import "./style.css";
 
-if (!loginState.isLoggedIn) {
-  window.location.replace("/login.html");
-}
-// const app = document.querySelector<HTMLDivElement>("#app");
-// app!.innerHTML = "<h1>Hello Typescript</h1>";
+const refreshAccessToken = async () => {
+  try {
+    const response = await fetch("http://127.0.0.1:8000/refresh/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+
+    console.log("Response status : ", response.status);
+    console.log("Response headers : ", response.headers);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+  } catch (error) {
+    console.error("Error refreshing token", error);
+  }
+};
+
+refreshAccessToken().then((date) => {
+  loginState.isLoggedIn = true;
+  // loginState.user = {};
+
+  if (!loginState.isLoggedIn) {
+    window.location.replace("/login.html");
+  } else {
+    window.location.replace("/dashboard.html");
+  }
+});
--- src/types/global.ts
+++ src/types/global.ts
@@ -1,9 +1,7 @@
 export interface UserType {
   id: number;
   email: string;
-  password: string;
   last_login: string | null; // Nullable date field
-  is_superuser: boolean;
   username: string;
   first_name: string;
   last_name: string;
